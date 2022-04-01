acea3ad22f92eea3ec26516e911650718f64c9cc
Griselda Quipildor <gri.quipildor@gmail.com>
2022-04-01 17:17:07 -0300

    watchers

--- watchers/watchers.js
+++ watchers/watchers.js
@@ -5,8 +5,27 @@ new Vue({
         estado: "Introduce nueva contraseña",
         novalidos:[
             "admin",
-            "123",
+            "12345",
             "password"
-        ]
+        ],
     },
+    watch: {
+        password(nuevo, antiguo){
+            //indexOf retorna el primer índice en el que se puede encontrar un elemento dado en el array, ó retorna -1 si el elemento no esta presente.
+
+                //que no forme parte de los password invalidos
+            if (this.novalidos.indexOf(nuevo) > -1) {
+                this.estado = "Contraseña no válida";
+                return false;
+            }
+                //que los caracteres sean mayores a 5
+            if (nuevo.length < 5) {
+                this.estado = "La contraseña debe tener al menos 5 caracteres ";
+                return false;
+            }
+
+            this.estado = "Contraseña correcta";
+        }
+    }
 })
+    Vue.config.devtools = true;
